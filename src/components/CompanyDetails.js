1dcb7519e2f098186eae66d715a1a151fd24b605
hcarine <hammescarine@gmail.com>
2019-07-24 00:07:53 +0200

    show with a card the company details

--- src/components/CompanyDetails.js
+++ src/components/CompanyDetails.js
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
-import Input from '@material-ui/core/Input';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 const CompanyDetails = (props) => {
-    const constructDetails = () => {
+    const constructProjects = () => {
         return props.result.map(project=>
             (
-             <ul> 
-                 <label>name: {project.name} </label>
-                <li>language:{ project.language}</li> 
-              </ul>  
+            <Card>
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {project.name}
+                  </Typography>
+                  <Typography color="textSecondary">
+                    { project.language}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    { project.stars}
+                  </Typography>
+                </CardContent>
+              </Card>
             ))
     };
 
     return (
         <>
-            {props.result.length && constructDetails()}
+       { props.result.length &&
+        <div>
+            <h1>Name: {props.result[0].owner.login}</h1>
+            <h1>gravatar: {props.result[0].owner.avatar_url}</h1>
+        </div>
+        }
+        <div>Projects</div>
+           {  props.result.length && constructProjects()}
+           
         </>
     );
 }
