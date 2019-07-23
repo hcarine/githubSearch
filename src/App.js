4b654788af560e23293ec12f3bddeb5f839f887f
hcarine <hammescarine@gmail.com>
2019-07-23 23:42:50 +0200

    final stricture apiCalls

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import SearchProject from './components/SearchProject'
+import {getCompanyProject} from './client/api'
 
 function App() {
+  const [result, setResult] = useState("")
+
+  const callApi = (text) => {
+    getCompanyProject(text, setResult)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Github Search
       </header>
-      <SearchProject/>
-        
+      <SearchProject callApi={callApi}/>
+      <div>
+        {result.length}
+      </div>
     </div>
   );
 }
--- src/components/SearchProject.js
+++ src/components/SearchProject.js
@@ -1,28 +1,19 @@
 import React, { useState } from 'react';
 import Input from '@material-ui/core/Input';
-import {getCompanyProject} from '../client/api'
 
 const SearchProject = (props) => {
     const [project, setProject] = useState("")
-    const [result, setResult] = useState("")
+
     const chageInput = (event) => {
         const projectName = event.target.value
         setProject(projectName)
         if(projectName.length >= 3 ){
-            callApi(projectName)
+            props.callApi(projectName)
         }
     };
-  const callApi = (text) => {
-    getCompanyProject(text,setResult)
-  }
 
     return (
-        <>
         <Input value={project} type="text" onChange={chageInput}/>
-        <div>
-            {result.length}
-        </div>
-        </>
     );
 }
 
