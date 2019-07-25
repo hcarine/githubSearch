62d1d4aa8cba895a05a2bcc532e3e6e647395a1c
hcarine <hammescarine@gmail.com>
2019-07-25 20:23:58 +0200

    add company avatar

--- src/components/CompanyDetails.js
+++ src/components/CompanyDetails.js
@@ -3,14 +3,15 @@ import ProjectCards from './ProjectCards'
 
 const CompanyDetails = (props) => {
     return  props.result.length  ? (
-        <>
         <div>
-            <h1>Name: {props.result[0].owner.login}</h1>
-            <h1>gravatar: {props.result[0].owner.avatar_url}</h1>
+            <h1>
+                <img src={props.result[0].owner.avatar_url} alt="company avatar" height="42" width="42"></img>
+                 {props.result[0].owner.login}
+            </h1>
+
+            <h2>Projects</h2>
+            <ProjectCards company={props.result}/>
         </div>
-        <div>Projects</div>
-        <ProjectCards company={props.result}/>
-        </>
        ): 
        <div>
            The company does not exist
