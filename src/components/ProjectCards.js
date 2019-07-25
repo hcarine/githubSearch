90620f3dd587a649e22f960fe1ca85b8885310ae
hcarine <hammescarine@gmail.com>
2019-07-25 20:18:57 +0200

    adjust cardStyle

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -3,12 +3,18 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
+const cardStyle = {
+  'width':'10%',
+  'display':'inline-block',
+  'margin': '10px',
+}
+
 const ProjectCards = (props) => {
     return props.company.map(project=>
         (
-        <Card>
+        <Card style={cardStyle}>
             <CardContent>
-              <Typography variant="h5" component="h2">
+              <Typography variant="h6" component="h4">
                 {project.name}
               </Typography>
               <Typography color="textSecondary">
